235a766276915bf68e231d09a6c02bb6c26ea894
unknown <kcy9442@gmail.com>
2026-08-24 12:32:46 +0900

    260824:v1

--- dambda-bootstrap/oidc.tf
+++ dambda-bootstrap/oidc.tf
@@ -71,8 +71,8 @@ resource "aws_iam_policy" "core" {
           "dynamodb:DeleteItem"
         ]
         Resource = [
-          "arn:aws:s3:::dambda-bootstrap-bucket",
-          "arn:aws:s3:::dambda-bootstrap-bucket/*",
+          "arn:aws:s3:::dambda-bootstrap3-bucket",
+          "arn:aws:s3:::dambda-bootstrap3-bucket/*",
           "arn:aws:dynamodb:ap-northeast-2:${local.account_id}:table/terraform-lock-table"
         ]
       },
