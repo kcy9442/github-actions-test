92a6eaebe0387c102985fb7c83211fe9df943637
kcy9442 <kcy9442@gmail.com>
2026-08-26 11:29:01 +0900

    fix: use S3 lockfile for Terraform state
    Updated backend configuration to use lockfile for state management.

--- dambda/backend.tf
+++ dambda/backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "dambda-bootstrap-469072180472-tfstate"
-    key            = "path/to/my/key/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
+    bucket       = "dambda-bootstrap-469072180472-tfstate"
+    key          = "path/to/my/key/terraform.tfstate"
+    region       = "ap-northeast-2"
+    use_lockfile = true
+    encrypt      = true
   }
 
-  # The S3 bucket and DynamoDB table are managed by dambda-bootstrap.
+  # The S3 bucket is managed by dambda-bootstrap.
 }
